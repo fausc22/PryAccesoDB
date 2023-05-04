5a6adb8a19db6ae0819986737d3ec1e9e7330da9
root <mgiraudo@ies21.edu.ar>
2023-05-04 13:57:07 -0300

    uwu

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,6 +31,10 @@ namespace PryAccesoDB
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btnConectar = new System.Windows.Forms.Button();
+            this.btnLeer = new System.Windows.Forms.Button();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -44,7 +48,7 @@ namespace PryAccesoDB
             // 
             // btnConectar
             // 
-            this.btnConectar.Location = new System.Drawing.Point(271, 135);
+            this.btnConectar.Location = new System.Drawing.Point(282, 53);
             this.btnConectar.Name = "btnConectar";
             this.btnConectar.Size = new System.Drawing.Size(163, 60);
             this.btnConectar.TabIndex = 1;
@@ -52,15 +56,44 @@ namespace PryAccesoDB
             this.btnConectar.UseVisualStyleBackColor = true;
             this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
             // 
+            // btnLeer
+            // 
+            this.btnLeer.Location = new System.Drawing.Point(282, 119);
+            this.btnLeer.Name = "btnLeer";
+            this.btnLeer.Size = new System.Drawing.Size(163, 60);
+            this.btnLeer.TabIndex = 2;
+            this.btnLeer.Text = "LEER TABLA";
+            this.btnLeer.UseVisualStyleBackColor = true;
+            this.btnLeer.Click += new System.EventHandler(this.btnLeer_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dgvDatos.Location = new System.Drawing.Point(76, 231);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(612, 150);
+            this.dgvDatos.TabIndex = 3;
+            this.dgvDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatos_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "libro";
+            this.Column1.Name = "Column1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.btnLeer);
             this.Controls.Add(this.btnConectar);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -70,6 +103,9 @@ namespace PryAccesoDB
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnLeer;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -23,6 +23,8 @@ namespace PryAccesoDB
 
         //declaracion de variables globales para este formulario
         OleDbConnection conn = null;
+        OleDbCommand cmd = null;
+        OleDbDataReader dr = null;
 
 
         private void btnConectar_Click(object sender, EventArgs e)
@@ -30,6 +32,28 @@ namespace PryAccesoDB
             conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=DB.accdb;");
             conn.Open();
             MessageBox.Show("Conexion a base de datos establecida");
+        }
+
+        private void btnLeer_Click(object sender, EventArgs e)
+        {
+            cmd = new OleDbCommand();
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.TableDirect;
+            cmd.CommandText = "TITULO";
+
+            //MessageBox.Show("Conexion a " + cmd.CommandText);
+
+            dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                dgvDatos.Rows.Add(dr[0]);
+            }
+        }
+
+        private void dgvDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
