d4c9194319a945b38b3c0d6d15a7664c73ee90fc
root <mgiraudo@ies21.edu.ar>
2023-05-04 13:26:17 -0300

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//importar la libreria de ADO
+using System.Data.OleDb;
+
+
+
+namespace PryAccesoDB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //declaracion de variables globales para este formulario
+        OleDbConnection conn = null;
+
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=DB.accdb;");
+            conn.Open();
+            MessageBox.Show("Conexion a base de datos establecida");
+        }
+    }
+}
